727010f25f336d80e4dd3feb72cdb1e9045b6bea
ispong <ispong@outlook.com>
2026-06-14 11:58:56 +0800

    优化路由

--- spark-yun-backend/spark-yun-api/src/main/java/com/isxcode/spark/api/user/constants/RoleType.java
+++ spark-yun-backend/spark-yun-api/src/main/java/com/isxcode/spark/api/user/constants/RoleType.java
@@ -1,23 +1,37 @@
 package com.isxcode.spark.api.user.constants;
 
-/** 角色. */
+/**
+ * 角色.
+ */
 public interface RoleType {
 
-    /** 平台超级管理员. */
-    String platformSuperAdmin = "ROLE_SYS_ADMIN";
+    /**
+     * 平台超级管理员.
+     */
+    String PLATFORM_SUPER_ADMIN = "PLATFORM_SUPER_ADMIN";
 
-    /** 平台管理员. */
-    String platformAdmin = "ROLE_PLATFORM_ADMIN";
+    /**
+     * 平台管理员.
+     */
+    String PLATFORM_ADMIN = "PLATFORM_ADMIN";
 
-    /** 租户超级管理员. */
-    String tenantSuperAdmin = "ROLE_TENANT_ADMIN";
+    /**
+     * 租户超级管理员.
+     */
+    String TENANT_SUPER_ADMIN = "TENANT_SUPER_ADMIN";
 
-    /** 租户管理员. */
-    String tenantAdmin = "ROLE_TENANT_NORMAL_ADMIN";
+    /**
+     * 租户管理员.
+     */
+    String TENANT_ADMIN = "TENANT_ADMIN";
 
-    /** 普通成员. */
-    String tenantMember = "ROLE_TENANT_MEMBER";
+    /**
+     * 普通成员.
+     */
+    String TENANT_MEMBER = "TENANT_MEMBER";
 
-    /** 匿名者权限. */
+    /**
+     * 匿名者权限.
+     */
     String ROLE_ANONYMOUS = "ROLE_ANONYMOUS";
 }
