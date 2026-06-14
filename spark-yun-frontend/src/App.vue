5c2713dd000f934e608576ea9aac38ad4db7d5fc
ispong <ispong@outlook.com>
2026-06-14 09:40:45 +0800

    优化App.vue

--- spark-yun-frontend/src/App.vue
+++ spark-yun-frontend/src/App.vue
@@ -1,12 +1,15 @@
 <template>
     <el-config-provider :locale="locale">
+        <!-- 进入路由 -->
         <router-view />
     </el-config-provider>
 </template>
+
 <script lang="ts" setup>
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import { reactive } from 'vue'
 
+// 设置elementPlus的语言
 const { locale } = reactive({
     locale: zhCn
 })
